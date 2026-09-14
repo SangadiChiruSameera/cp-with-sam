f4aacbd7db566fc30a29eaf8156959563f6591b4
Sameera Sangadi <sameerasangadi@gmail.com>
2026-09-14 14:58:08 +0530

    630A-Again Twenty Five!

--- AAgainTwentyFive.java
+++ AAgainTwentyFive.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class AAgainTwentyFive{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        //long n=sc.nextInt();
         System.out.println("25");
     }
 }
