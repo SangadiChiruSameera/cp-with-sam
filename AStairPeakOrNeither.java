1d38882fcad35652738047601004dbba74cf0bcf
Sameera Sangadi <sameerasangadi@gmail.com>
2026-09-17 17:30:23 +0530

    1950A-Stair,Peak,orNeither

---
+++ AStairPeakOrNeither.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class AStairPeakOrNeither{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a<b && a<c && b<c ){
+                System.out.println("STAIR");
+            }else if(a<b && b>c){
+                System.out.println("PEAK");
+            }else{
+                System.out.println("NONE");
+            }
+        }
+    }
+} 
