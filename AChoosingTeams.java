0cbd2d994966c8caad6ce5bc03d261385552e630
Sameera Sangadi <sameerasangadi@gmail.com>
2026-09-19 21:06:06 +0530

    432A-Choosing Teams

---
+++ AChoosingTeams.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class AChoosingTeams{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]+k<=5){
+                count++;
+            }
+        }
+        System.out.println(count/3);
+    }
+}
