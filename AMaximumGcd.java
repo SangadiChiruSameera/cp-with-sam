9786ac0769025b4b8cc118b2c038f90f8d8a5af9
Sameera Sangadi <sameerasangadi@gmail.com>
2026-09-19 10:26:11 +0530

    1370A-Maximum Gcd(simple intution)

--- AMaximumGcd.java
+++ AMaximumGcd.java
@@ -5,24 +5,7 @@ public class AMaximumGcd{
         int t=sc.nextInt();
         while(t-->0){
             int n=sc.nextInt();
-            if(n==1){
-                System.out.println(1);
-                break;
-            }
-            int maxgcd=1;
-            for(int i=2;i<=n;i++){
-                for(int j=2;j<=i;j++){
-                    if(i==j) continue;
-                    maxgcd=Math.max(maxgcd,gcd(i,j));
-                }
-            }
-            System.out.println(maxgcd);
+            System.out.println(n/2);
         }
-    }
-    public static int gcd(int a,int b){
-        if(a==0){
-            return b;
-        }
-        return gcd(b%a,a);
     }
 }
