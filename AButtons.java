7f355f937934c1719b6d2a530236215e4e8a95ef
Sameera Sangadi <sameerasangadi@gmail.com>
2026-09-20 16:35:26 +0530

    1858A-Buttons(optimal code if condition)

--- AButtons.java
+++ AButtons.java
@@ -1,28 +1,22 @@
 import java.util.*;
-public class AButtons{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            int c=sc.nextInt();
-            int n=a+b+c;
-            boolean anna=true;
-            while(n-->0){
-                if(anna && a>0){
-                    a--;
-                }else if(!anna && b>0){
-                    b--;
-                }else{
-                    c--;
-                }
-                anna=!anna; 
-            }
-            if(!anna){
+
+public class AButtons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a > b) {
                 System.out.println("First");
-            }else{
+            } else if (a < b) {
                 System.out.println("Second");
+            } else {
+                System.out.println(c % 2 == 1 ? "First" : "Second");
             }
         }
     }
