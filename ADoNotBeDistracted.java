f2405ef7b45d5c8d5b71f9f2062e25f75b2f1fed
Sameera Sangadi <sameerasangadi@gmail.com>
2026-09-25 22:24:25 +0530

    1520A-DO NOT BE DISTRACTED!

--- ADoNotBeDistracted.java
+++ ADoNotBeDistracted.java
@@ -1,28 +1,37 @@
 import java.util.*;
-public class ADoNotBeDistracted{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            String str=sc.next();
-            int idx=0;
-            boolean found=false;
-            HashMap<Character,Integer> map=new HashMap<>();
-            while(idx<n){
-                while(idx>0 && str.charAt(idx)==str.charAt(idx-1)){
-                    idx++;
+
+public class ADoNotBeDistracted {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String str = sc.next();
+
+            boolean[] seen = new boolean[26];
+            boolean suspicious = false;
+
+            for (int i = 0; i < n; i++) {
+
+                // Start of a new task block
+                if (i == 0 || str.charAt(i) != str.charAt(i - 1)) {
+                    int idx = str.charAt(i) - 'A';
+
+                    // This task was already completed before
+                    if (seen[idx]) {
+                        suspicious = true;
+                        break;
+                    }
+
+                    seen[idx] = true;
                 }
-                char ch=str.charAt(idx);
-                if(map.containsKey(ch) && map.get(ch)>0){
-                    System.out.println("NO");
-                    found=true;
-                    break;
-                }
-                map.put(ch,map.getOrDefault(ch,0)+1);
-                idx++;
             }
-            if(!found) System.out.println("YES");
+
+            System.out.println(suspicious ? "NO" : "YES");
         }
+
+        sc.close();
     }
 }
